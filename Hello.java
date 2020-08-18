4679a655da9665f3009c219b3548f52a9ba7dbe2
nishaprayaga <40233498+nishaprayaga@users.noreply.github.com>
2020-08-18 13:19:45 +0100

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -4,7 +4,7 @@ public static void main (String[] args)
 {
 for(int i=1;i<=10;i++)
 {
-System.out.println("Hello World is here again.. "+i);
+System.out.println("Hello World is here again so rock.. "+i);
 }
 }
 }
